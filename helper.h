4cd02c52eec0f8be6543b1dd828df8bd45f6e676
Nabeel <abdulnabeel40@gmail.com>
2021-12-05 19:44:49 +0500

    separated checkEntry function

--- helper.h
+++ helper.h
@@ -1,4 +1,7 @@
 #include <iostream>
 using namespace std;
+
 const int row = 3, column = 3;
-void showBoard(int board[row][column]);
+bool checkplace(int playerEntry);
+void showBoard(char board[row][column]);
+char board[row][column];
--- main.cpp
+++ main.cpp
@@ -1,22 +1,38 @@
 /* what i need to make tictactoe
-1. board
-2. players (user and a computer)
-3. algorithm to register move
+1.(done) board
+2. (done)players (user and a computer)
+3. (only done for user bot is still remaining)algorithm to register move
 4. check how wins 
 5. run until someone wins
 */
-#include <helper.h>
-
+#include "helper.h"
+int player, bot;
+bool checkplace(int playerEntry);
 int main()
 {
-    int board[row][column];
+
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < column; j++)
         {
-            board[i][j] = 0;
+            board[i][j] = '-';
         }
     }
+    do
+    {
+        showBoard(board);
+        cout << "Enter (1-9): ";
 
-    showBoard(board);
+        while (true)
+        {
+            cin >> player;
+            player--;
+            if (checkplace(player) == true)
+            {
+                board[player / row][player % column] = 'X';
+                break;
+            }
+        }
+
+    } while (true);
 }
