cda692f78f16ba8889ef2d0ea60d1706d3f32dcc
Nabeel <abdulnabeel40@gmail.com>
2021-12-05 18:50:30 +0500

    made a board

--- main.cpp
+++ main.cpp
@@ -5,3 +5,36 @@
 4. check how wins 
 5. run until someone wins
 */
+#include <iostream>
+using namespace std;
+
+const int row = 3, column = 3;
+
+void showBoard(int board[row][column]);
+int main()
+{
+    int board[row][column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            board[i][j] = 0;
+        }
+    }
+
+    showBoard(board);
+}
+
+void showBoard(int board[row][column])
+{
+    for (int i = 0; i < row; i++)
+    {
+        cout << "---------------------" << endl;
+        for (int j = 0; j < column; j++)
+        {
+            cout << "|  " << board[i][j] << "  |";
+        }
+        cout << endl;
+    }
+    cout << "---------------------" << endl;
+}
